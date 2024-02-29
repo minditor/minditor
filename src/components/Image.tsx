ff93887544e4791238ed24c465b9c02d41bc4e9f
sskyy <skyking_H@hotmail.com>
2024-03-01 00:21:07 +0800

    feat: add url type to Image block

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -6,10 +6,10 @@ import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
 import ImageEditor from '@uppy/image-editor';
 import '@uppy/image-editor/dist/style.min.css';
-import {Component} from "../DocumentContent.js";
 import AwsS3 from '@uppy/aws-s3';
 import XHR from '@uppy/xhr-upload'
 import {AxiiComponent} from "../AxiiComponent.js";
+import {Input} from "../lib/Input.js";
 
 type ImageData = {
     src: string
@@ -56,7 +56,7 @@ const uploaderTypes = {
 export function createImageBlock(uploadType: keyof typeof uploaderTypes, config: any) {
     return class ImageBlock extends AxiiComponent {
         static displayName = 'Image'
-        public element?: HTMLElement
+        public uppyContainer?: HTMLElement
         public uppy? : Uppy
         public src: Atom<string>
         constructor(public data: ImageData) {
@@ -73,7 +73,7 @@ export function createImageBlock(uploadType: keyof typeof uploaderTypes, config:
                 })
                 // @ts-ignore
                 .use(uploaderTypes[uploadType]!, config)
-                .use(Dashboard, { inline: true, target: this.element, proudlyDisplayPoweredByUppy:false, height:300 })
+                .use(Dashboard, { inline: true, target: this.uppyContainer, proudlyDisplayPoweredByUppy:false, height:300 })
                 .use(ImageEditor, { target: Dashboard })
                 .on('upload-success', (file, response) => {
                     // 更新自己
@@ -81,25 +81,78 @@ export function createImageBlock(uploadType: keyof typeof uploaderTypes, config:
                 });
             }
         }
-        destroy() {
-            super.destroy();
-            delete this.uppy
+        file: Atom<any> = atom(null)
+        url: Atom<''> = atom('')
+        onUseUrl = () => {
+            this.src(this.url())
+        }
+        onFileChange = (e: InputEvent) => {
+            // @ts-ignore
+            const fileObject = e.target.files[0]
+            this.file(fileObject)
+            this.uppy?.addFile({name: fileObject.name, type:fileObject.type, data: fileObject})
         }
         renderContainer() {
-            return <div style={{display: 'block'}} contenteditable={false}></div>
+            return <div  contenteditable={false}></div>
         }
         renderInner() {
+            const buttonStyle = {
+                display: 'inline-block',
+                textAlign: 'center',
+                padding: '10px',
+                background:'#000',
+                color: '#fff',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                outline:0,
+                borderWidth: 0
+            }
+
+            const uppyStyle = () => ({
+                display: this.file() ? 'block' : 'none',
+            })
+
+            const insertContainerStyle = () => {
+                return this.src() ? {} : {
+                    display: 'block',
+                    textAlign: 'center',
+                    padding: '20px',
+                    border: '2px dashed #ccc',
+                    borderStyle: 'dashed',
+                    borderRadius: '5px'
+                }
+            }
+
             return <>
-                {() => !this.src() ? (this.element = <div className="image-uppy-root" contenteditable={false}></div> as  HTMLElement): null }
+                {() => !this.src() && !this.file() ? (
+                    <div style={insertContainerStyle}>
+                        <div>
+                            <input type="file" name={`${this.id}_file`} id={`${this.id}_file`} style={{position:'absolute', width:'0.1px', height:0, opacity:0, overflow:'hidden', zIndex:-1}} onChange={this.onFileChange} />
+                            <label for={`${this.id}_file`} style={buttonStyle}>Upload File</label>
+                        </div>
+                        <div style={{padding:10, color: '#aaa'}}>or</div>
+                        <div style={{display:'flex', justifyContent: 'center'}}>
+                            <Input value={this.url} placeholder='image url' />
+                            <button style={{marginLeft: 8, ...buttonStyle}} onClick={this.onUseUrl}>Insert</button>
+                        </div>
+                    </div>
+                ) : null}
+                {() => !this.src() ?
+                    (this.uppyContainer = <div style={uppyStyle} className="image-uppy-root" contenteditable={false}></div> as  HTMLElement)
+                    : null
+                }
                 {
                     () => this.src() ? (<div className="image-preview-root" style={{textAlign: "center"}} contenteditable={false}>
-                        <img style={{maxWidth: '80%', width: 'auto',   }} src={this.src()}
+                        <img style={{maxWidth: '80%', width: 'auto'}} src={this.src()}
                              alt={this.data.alt}/>
                     </div>) :null
                 }
             </>
         }
-
+        destroy() {
+            super.destroy();
+            delete this.uppy
+        }
         toJSON(): any {
             console.log('toJSON',{
                 ...super.toJSON(),
